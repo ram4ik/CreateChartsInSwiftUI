fafac746b58923dcda640d03ddbf4f60e939c519
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-13 17:37:25 +0200

    project init

--- CreateChartsInSwiftUI/ContentView.swift
+++ CreateChartsInSwiftUI/ContentView.swift
@@ -6,11 +6,53 @@
 //
 
 import SwiftUI
+import SwiftUICharts
+
+struct ChartViews: View {
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            LineChartView(data: [11, 32, 4, 5, 8, 9, 23], title: "Line Chart")
+                .padding()
+            
+            BarChartView(data: ChartData(values: [
+                ("Jan", 12),
+                ("Feb", 2),
+                ("Mar", 6),
+                ("Apr", 9),
+                ("May", 10),
+                ("Jun", 2),
+            ]), title: "Bar Chart")
+            .padding()
+            
+            PieChartView(data: [23, 42, 12, 5, 6, 13], title: "Pie Chart")
+                .padding()
+        }
+    }
+}
+
+struct LineViews: View {
+    var body: some View {
+        ScrollView {
+            LineView(data: [12, 10, 9, 23, 4, 9, 13, 5, 19, 8])
+                .padding()
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            ChartViews()
+                .tabItem {
+                    Image(systemName: "list.dash")
+                    Text("Menu")
+                }
+            LineViews()
+                .tabItem {
+                    Image(systemName: "square.and.pencil")
+                    Text("Order")
+                }
+        }
     }
 }
 
