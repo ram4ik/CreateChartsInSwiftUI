5e0bd345b8a66c0ca9f2c658d116445569741889
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-13 16:41:47 +0200

    Initial Commit

---
+++ CreateChartsInSwiftUI/CreateChartsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateChartsInSwiftUIApp.swift
+//  CreateChartsInSwiftUI
+//
+//  Created by ramil on 13.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct CreateChartsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
